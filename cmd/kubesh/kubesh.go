fd9c04e7e8fd191e180205cdfc27eb9a02a713dd
Jim Crossley <jim@crossleys.org>
2016-09-28 14:46:42 -0400

    Minor fixes #12

--- cmd/kubesh/kubesh.go
+++ cmd/kubesh/kubesh.go
@@ -72,10 +72,10 @@ func main() {
 	}
 
 	fmt.Println("Welcome to kubesh, the kubectl shell!")
-	fmt.Println("Type 'help' or TAB to see available commands")
+	fmt.Println("Type 'help' or <TAB> to see available commands")
 	fmt.Println("For options/flags, tab complete a dash, '--<TAB>'")
 	fmt.Println("Use 'pin' when multiple commands apply to same resource")
-	fmt.Println("Use GNU readline key bindings for editing and history")
+	fmt.Println("Use GNU readline key bindings for command editing and history")
 	for {
 		line, err := sh.rl.Readline()
 		if err == readline.ErrInterrupt {
